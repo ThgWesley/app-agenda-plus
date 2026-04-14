d24d330a9721dda4df9b7c214c7a228f9c03e293
ThgWesley <thiagow031@gmail.com>
2026-04-14 01:18:36 -0300

    versão final

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agenda-plus-v1';
+const CACHE_NAME = 'agenda-plus-v3';
 const ASSETS = [
     './',
     './index.html',
@@ -9,13 +9,25 @@ const ASSETS = [
     './js/calendar.js',
     './js/finance.js',
     './js/report.js',
-    './manifest.json'
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
     );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
